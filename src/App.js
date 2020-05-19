b7a91041e1a5c7692cef651369a9e4a02c37e769
Juraj Basanda <basanda.juraj@gmail.com>
2020-05-19 12:31:42 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React,{useEffect} from 'react';
+import React from 'react';
 import { BrowserRouter as BRouter,Switch,Route } from "react-router-dom";
-import {  animateScroll as scroll } from "react-scroll";
+
 
 
 import './App.scss';
@@ -14,11 +14,12 @@ import NoMatch from './NoMatch';
 
 function App() {
   //Scoll up after clicked on service
-  useEffect(() => {
-    scroll.scrollToTop();
-  })
-  
+  //useEffect(() => {
+    //scroll.scrollToTop();
+  //})
+
   return (
+
     <>
     <BRouter>
       <Nav/>
@@ -33,38 +34,35 @@ function App() {
           </Route>
         </Switch>
     </BRouter>
-   
-    
     </>
-  );
+
+
+  )
+  
 }
 
 const Home = () => {
 
-  //Scoll up after clicked on service
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-
-  
-
-  return(
-    <>
+  return (
+      <>
     <header id="about">
         <section className="head " >
             <div className="name ">
                 <h1 className="color">HEY, I'M JURAJ BASANDA</h1>
                 <h2 className="content">FRONT-END WEB DEVELOPER</h2>
             </div>
-            <p className="content">As passionate and creative developer I’m building sleek websites with the modern technologies. 
+            <p className="content" mt="45">As passionate and creative developer I’m building sleek websites with the modern technologies. 
                 I'm currently seeking out the right team where I can contribute in making amazing digital products.
             </p>
         </section>
+        
     </header>
      <Portfolio/>
      <Contact/>
+
      </>
     
+
   )
 }
 
